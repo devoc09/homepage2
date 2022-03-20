cb3814306b9c2d5cd0ecbc304179b8d844414e38
devoc09 <takudefo@gmail.com>
2022-03-21 06:08:57 +0900

    delete unnecessary code.

--- server/http/handler.go
+++ server/http/handler.go
@@ -1,13 +1,7 @@
 package http
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
-
-func indexHandler() echo.HandlerFunc {
-	return func(c echo.Context) (err error) {
-		return c.HTML(http.StatusOK, "public/index.html")
-	}
-}
+// func indexHandler() echo.HandlerFunc {
+// 	return func(c echo.Context) (err error) {
+// 		return c.HTML(http.StatusOK, "public/index.html")
+// 	}
+// }
