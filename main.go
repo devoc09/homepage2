d75891619888ea7a1232ffccb8bc3c1f3c306c70
devoc09 <takudefo@gmail.com>
2022-10-12 21:13:04 +0900

    delete script and deploy pages

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/devoc09/homepage2/server"
-
-func main() {
-	server.Run()
-}
--- server/http/handler.go
+++
@@ -1,7 +0,0 @@
-package http
-
-// func indexHandler() echo.HandlerFunc {
-// 	return func(c echo.Context) (err error) {
-// 		return c.HTML(http.StatusOK, "public/index.html")
-// 	}
-// }
